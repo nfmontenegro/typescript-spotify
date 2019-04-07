60386af7aaa4c8456897ea22f427067c28526bdd
Nicolás <nicolas.fmontenegro@gmail.com>
2019-04-06 22:08:02 -0300

    init project

---
+++ server/lib/core/spotifyCredentials.ts
@@ -0,0 +1,32 @@
+import axios, {AxiosRequestConfig, AxiosResponse, AxiosPromise} from 'axios'
+
+async function authenticate(): Promise<AxiosPromise> {
+  try {
+    const URL: any = 'https://accounts.spotify.com/api/token'
+    const CLIENT_ID: any = 'bcf049a8882c4be597b4ff7019c61807'
+    const CLIENT_SECRET: any = 'e6ea4b89ad964ab2b0d7389b14ece51f'
+
+    const options: AxiosRequestConfig = {
+      url: URL,
+      method: 'POST',
+      params: {
+        grant_type: 'client_credentials'
+      },
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      auth: {
+        username: CLIENT_ID,
+        password: CLIENT_SECRET
+      }
+    }
+
+    const response: AxiosResponse = await axios(options)
+
+    console.log(response)
+    return response.data
+  } catch (err) {
+    console.log('Error:', err)
+  }
+}
