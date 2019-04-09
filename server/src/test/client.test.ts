781826f1a536538cf7d4a9bf79a8d299c6814b87
Nicolás <nicolas.fmontenegro@gmail.com>
2019-04-08 20:44:21 -0400

    test credentials

--- server/src/test/client.test.ts
+++ server/src/test/client.test.ts
@@ -4,7 +4,8 @@ import {authenticate} from '../lib/core/spotifyCredentials'
 
 describe('# Credentials', () => {
   test('Get credentials user', async () => {
-    const response = await axios('http://localhost:3000')
-    console.log('response', response)
+    const response = await axios('http://localhost:3000/', {method: 'GET'})
+    expect(response.status).toBe(200)
+    expect(response.statusText).toBe('OK')
   })
 })
