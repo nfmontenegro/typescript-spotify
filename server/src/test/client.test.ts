a79cd431f6877e31be8d089cbbaec887bf40739e
Nicolás <nicolas.fmontenegro@gmail.com>
2019-04-10 14:30:36 -0400

    environment

--- server/src/test/client.test.ts
+++ server/src/test/client.test.ts
@@ -1,10 +1,11 @@
 import axios, {AxiosResponse} from 'axios'
 
-import {authenticate} from '../lib/core/spotifyCredentials'
-
 describe('# Credentials', () => {
   test('Get credentials user', async () => {
-    const response = await axios('http://localhost:3000/', {method: 'GET'})
+    const response: AxiosResponse = await axios(process.env.HOST, {
+      method: 'GET'
+    })
+
     expect(response.status).toBe(200)
     expect(response.statusText).toBe('OK')
   })
